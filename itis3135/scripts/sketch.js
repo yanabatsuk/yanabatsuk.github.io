f107e269e681674fc78682125f601f84bb3b1791
Yana Batsuk <168874208+yanabatsuk@users.noreply.github.com>
2024-12-03 10:34:34 -0500

    p5.js update page

---
+++ itis3135/scripts/sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+    // Create canvas
+    createCanvas(710,400);
+
+    //Set background to black
+    background(0);
+
+    // Set width of the strokes(lines)
+    strokeWeight(10);
+
+    // Set color mode to hue-saturation-brightness (HSB)
+    colorMode(HSB);
+
+    // Set screen reader accessible description
+    describe('A blank canvas where theuser draws by draggin the mosue');
+}
+
+function mouseDragged(){
+    // Set the color based on the mouse position, and draw a line
+    // from the previous position to the curret position
+    let lineHue = mouseX - mouseY;
+    stroke(lineHue, 100, 100);
+    lineHue(pmouseX, pmouseY, mouseX, mouseY);
+}
