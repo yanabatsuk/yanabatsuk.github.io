9b59015e5f8cca84740e1829b5d3beb79c41ee3b
Yana Batsuk <168874208+yanabatsuk@users.noreply.github.com>
2024-12-03 23:15:25 -0500

    Update script.js

--- itis3135/project/scripts/script.js
+++ itis3135/project/scripts/script.js
@@ -2,23 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
     // test to check it works
     console.log("Website Loaded");
 
-    // Client info. to dynamically load
-    const clientName = "Ethan Orange";
-    const clientCompany = "Ethanphotojuice";
-    const clientEmail = "ethanorange@gmail";
-    const clientPhone = "1+ (***) ***-****";
-
-
-    // contact info grabber
-    const contactSection = document.querySelector("#contact-info");
-
-    if (contactSection) {
-        contactSection.innerHTML += `
-        <p><strong>${clientName}</strong><br>${clientCompany}<br>
-        Email: <a href="mailto:${clientEmail}">${clientEmail}</a><br>
-        Phone: ${clientPhone}</p>`;
-    }
-
     // background images for cards, on packages page
     const backgroundImages = [
         'images/all-in-package.jpeg',
