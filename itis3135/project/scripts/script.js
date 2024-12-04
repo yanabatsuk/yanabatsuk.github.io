482b72aee3652b06d5a96cd672d4499934dd2d7a
Yana Batsuk <168874208+yanabatsuk@users.noreply.github.com>
2024-12-03 19:12:25 -0500

    updates

--- itis3135/project/scripts/script.js
+++ itis3135/project/scripts/script.js
@@ -5,13 +5,13 @@ document.addEventListener("DOMContentLoaded", () => {
     // Show the popup after the page loads
     window.onload = function() {
     setTimeout(function() {
-        document.getElementById("popupAd").style.display = "flex"; // Show the popup after a delay
+        document.getElementById("popupad").style.display = "flex"; // Show the popup after a delay
     }, 3000); // 3-second delay
     };
 
     // Close the popup when the close button is clicked
-    document.getElementById("closeBtn").onclick = function() {
-    document.getElementById("popupAd").style.display = "none";
+    document.getElementById("closebtn").onclick = function() {
+    document.getElementById("popupad").style.display = "none";
     };
 
     // Client info. to dynamically load
